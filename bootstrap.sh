027d6fc66d6f8557ac94d66f4b4774a19d34c97b
Chris Ellsworth <cellsworth@gmail.com>
2020-03-08 19:12:04 -0700

    p10k

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
